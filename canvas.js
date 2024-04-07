2234d9044fd62a77419df47d603d1a2c699c3721
Gorhe-Rohini <165666872+Gorhe-Rohini@users.noreply.github.com>
2024-04-07 09:44:53 -0700

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,6 @@
+var c = document.querySelector("myCanvas"); 
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight; 
+var c =canvas.getContext('2d');
+c.fillRect(0,0,200,100);
+console.log(canvas);
